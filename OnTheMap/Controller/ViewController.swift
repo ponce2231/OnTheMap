4556d20e3792a3b255193abe27777da1bb4677e5
Christopher Ponce Mendez <christopherponcedev@gmail.com>
2019-09-13 12:19:14 -0400

    added get students locations

--- OnTheMap/Controller/ViewController.swift
+++ OnTheMap/Controller/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ONTMClient.getAllStudentsLocations(completionHandler:getAllStudentsHandler(succes:error:))
     }
-
+    
+    func getAllStudentsHandler(succes: Bool, error: Error?){
+        if succes{
+            print("success")
+        }else{
+            print(error!)
+        }
+    }
 
 }
 
--- OnTheMap/Model/ONTMClient.swift
+++ OnTheMap/Model/ONTMClient.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+class ONTMClient {
+    
+    enum Endpoints {
+        static let base = "https://onthemap-api.udacity.com/v1/StudentLocation"
+        
+        case getStudentsLocation
+        
+        
+        var urlString:String {
+            switch self {
+            case .getStudentsLocation: return Endpoints.base + "?order=-updatedAt"
+
+            }
+
+        }
+        var url: URL {
+            return URL(string: urlString)!
+        }
+    }
+    
+    class func getAllStudentsLocations(completionHandler:@escaping (Bool,Error?) -> Void){
+        
+        
+        let dataTask = URLSession.shared.dataTask(with: Endpoints.getStudentsLocation.url) { data, response, error in
+            guard let data = data else{
+                completionHandler(false,error)
+                return
+            }
+            let decoder = JSONDecoder()
+            do{
+                let responseObject = try decoder.decode(StudentLocationRequest.self, from: data)
+                completionHandler(true,nil)
+            }catch{
+                completionHandler(false,error)
+            }
+            print(String(data: data, encoding: .utf8))
+        }
+        dataTask.resume()
+        
+    }
+    
+    
+}
